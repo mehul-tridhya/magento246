fd594dbfd6384c3aa23e1dede9f76f06d344da30
Mehul Parmar <mehul.tridhya@gmail.com>
2023-05-26 11:05:38 +0530

    Add required file using use method

--- app/code/Tridhyatech/ReturnManagement/Model/Rule.php
+++ app/code/Tridhyatech/ReturnManagement/Model/Rule.php
@@ -5,8 +5,17 @@ namespace Tridhyatech\ReturnManagement\Model;
 use Tridhyatech\ReturnManagement\Model\ResourceModel\Rule as RuleResourceModel;
 use Magento\Quote\Model\Quote\Address;
 use Magento\Rule\Model\AbstractModel;
+use Tridhyatech\ReturnManagement\Api\Data\ReturnRuleInterface;
+use Magento\Framework\Model\Context;
+use Magento\Framework\Registry;
+use Magento\Framework\Data\FormFactory;
+use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
+use Magento\CatalogRule\Model\Rule\Condition\CombineFactory as condCombineFactory;
+use Magento\SalesRule\Model\Rule\Condition\Product\CombineFactory as condProdCombineF;
+use Magento\Framework\Model\ResourceModel\AbstractResource;
+use Magento\Framework\Data\Collection\AbstractDb;
 
-class Rule extends AbstractModel
+class Rule extends AbstractModel implements ReturnRuleInterface
 {
     protected $_eventPrefix = 'dolphin_addconditionfiled';
     protected $_eventObject = 'rule';
@@ -17,14 +26,14 @@ class Rule extends AbstractModel
     protected $_displayProductIds;
 
     public function __construct(
-        \Magento\Framework\Model\Context $context,
-        \Magento\Framework\Registry $registry,
-        \Magento\Framework\Data\FormFactory $formFactory,
-        \Magento\Framework\Stdlib\DateTime\TimezoneInterface $localeDate,
-        \Magento\CatalogRule\Model\Rule\Condition\CombineFactory $condCombineFactory,
-        \Magento\SalesRule\Model\Rule\Condition\Product\CombineFactory $condProdCombineF,
-        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
-        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        Context $context,
+        Registry $registry,
+        FormFactory $formFactory,
+        TimezoneInterface $localeDate,
+        condCombineFactory $condCombineFactory,
+        condProdCombineF $condProdCombineF,
+        AbstractResource $resource = null,
+        AbstractDb $resourceCollection = null,
         array $data = []
     ) {
         $this->condCombineFactory = $condCombineFactory;
@@ -37,6 +46,115 @@ class Rule extends AbstractModel
         parent::_construct();
         $this->_init(RuleResourceModel::class);
         $this->setIdFieldName('rule_id');
+    }
+
+    public function getRuleId()
+    {
+        return $this->getData(self::RULE_ID);
+    }
+    public function setRuleId($ruleId)
+    {
+        return $this->getData(self::RULE_ID, $ruleId);
+    }
+
+
+    public function getRuleName()
+    {
+        return $this->getData(self::RULE_NAME);
+    }
+    public function setRuleName($ruleName)
+    {
+        return $this->getData(self::RULE_NAME, $ruleName);
+    }
+
+
+    public function getRuleStatus()
+    {
+        return $this->getData(self::RULE_STATUS);
+    }
+    public function setRuleStatus($ruleStatus)
+    {
+        return $this->getData(self::RULE_STATUS, $ruleStatus);
+    }
+
+
+    public function getPriority()
+    {
+        return $this->getData(self::PRIORITY);
+    }
+    public function setPriority($priority)
+    {
+        return $this->getData(self::PRIORITY, $priority);
+    }
+
+
+    public function getReturnPeriod()
+    {
+        return $this->getData(self::RETURN_PERIOD);
+    }
+    public function setReturnPeriod($returnPeriod)
+    {
+        return $this->getData(self::RETURN_PERIOD, $returnPeriod);
+    }
+
+
+    public function getRepairPeriod()
+    {
+        return $this->getData(self::REPAIR_PERIOD);
+    }
+    public function setRepairPeriod($repairPeriod)
+    {
+        return $this->getData(self::REPAIR_PERIOD, $repairPeriod);
+    }
+
+
+    public function getExchangePeriod()
+    {
+        return $this->getData(self::EXCHANGE_PERIOD);
+    }
+    public function setExchangePeriod($exchangePeriod)
+    {
+        return $this->getData(self::EXCHANGE_PERIOD, $exchangePeriod);
+    }
+
+
+    public function getDefaultResolutionPeriod()
+    {
+        return $this->getData(self::DEFAULT_RESOLUTION_PERIOD);
+    }
+    public function setDefaultResolutionPeriod($defaultResolutionPeriod)
+    {
+        return $this->getData(self::DEFAULT_RESOLUTION_PERIOD, $defaultResolutionPeriod);
+    }
+
+
+    public function getConditionsSerialized()
+    {
+        return $this->getData(self::CONDITIONS_SERIALIZED);
+    }
+    public function setConditionsSerialized($conditionsSerialized)
+    {
+        return $this->getData(self::CONDITIONS_SERIALIZED, $conditionsSerialized);
+    }
+
+
+    public function getActionsSerialized()
+    {
+        return $this->getData(self::ACTIONS_SERIALIZED);
+    }
+    public function setActionsSerialized($actionsSerialized)
+    {
+        return $this->getData(self::ACTIONS_SERIALIZED, $actionsSerialized);
+    }
+
+
+    public function getCreatedDate()
+    {
+        return $this->getData(self::CREATED_DATE);
+    }
+    public function setCreatedDate($createdDate)
+    {
+        return $this->getData(self::CREATED_DATE, $createdDate);
     }
 
     public function getConditionsInstance()
